d4cb3c709caef5497e659b1dff1c8acd20b8f34f
Issakov142 <82026984+Issakov142@users.noreply.github.com>
2024-10-06 00:58:59 +0500

    footer styling completed

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Icon } from '../../components/icon/Icon';
 import { FlexWrapper } from '../../components/FlexWrapper';
+import { theme } from '../../styles/Theme';
 
 export const Footer = () => {
   return (
@@ -37,15 +38,44 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-  background-color: #3683d5;
-  min-height: 20vh;
+  background-color: ${theme.colors.primaryBg};
+  padding: 40px 0;
 `;
 
-const Name = styled.span``;
+const Name = styled.span`
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: 700;
+  font-size: 22px;
+  letter-spacing: 0.14em;
+`;
 const SocialsList = styled.ul`
   display: flex;
-  gap: 30px;
+  gap: 20px;
+  margin: 30px 0;
 `;
 const SocialsItem = styled.li``;
-const SocialIconLink = styled.a``;
-const Copyright = styled.small``;
+const SocialIconLink = styled.a`
+  background: rgba(255, 255, 255, 0.1);
+  border-radius: 50%;
+  width: 35px;
+  height: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${theme.colors.accent};
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${theme.colors.accent};
+    color: ${theme.colors.primaryBg};
+
+    transform: translateY(-4px);
+  }
+`;
+const Copyright = styled.small`
+  font-weight: 400;
+  font-size: 12px;
+  text-align: center;
+
+  opacity: 0.5;
+`;
